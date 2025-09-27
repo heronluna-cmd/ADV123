0104b139cab981c775042d04905b1bfb1274a78c
heronluna-cmd <heron.luna@hcdc.edu.ph>
2025-09-27 18:00:45 +0800

    Update homepage content

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,16 +22,20 @@ export default function Home() {
           src="/next.svg"
           alt="Next.js logo"
           width={180}
+
           height={38}
           priority
         />
+
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
            Heron Luna
             
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save and see your changes instantly.origin  https://github.com/another/repo.git (fetch)
+origin  https://github.com/another/repo.git (push)
+
           </li>
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
